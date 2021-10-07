c5c31c26517bb80e2a4ef04487ddbc2222f7e4b2
Mikaela Suomalainen <mikaela+digitalents@mikaela.info>
2021-10-07 13:19:45 +0300

    flaskr/db.py: fix typo ged_db -> get_db

--- flaskr/db.py
+++ flaskr/db.py
@@ -21,7 +21,7 @@ def close_db(e=None):
         db.close()
 
 def init_db():
-    db = ged_db()
+    db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
